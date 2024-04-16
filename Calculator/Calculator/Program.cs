5d9dab12767fd5634f3508cbc02494a91c7bb1fa
LenkaSvob <LenkaLiberty@gmail.com>
2024-04-16 19:52:18 +0200

    Created simple calculator

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter mathematical operator; for addition (+), for subtraction (-), multiplication (*), division(/): ");
+            string @operator = Console.ReadLine();
+
+            Console.WriteLine("Enter second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double result;
+
+            if (@operator == "+") 
+            { 
+                result = num1 + num2;
+                Console.WriteLine("Result: {0} + {1} = {2}", num1,num2,num1 + num2);
+            }
+            else if (@operator == "-") 
+            { 
+                result = num1 - num2;
+                Console.WriteLine("Result: {0} - {1} = {2}", num1, num2, num1 - num2);
+            }
+            else if (@operator == "*") 
+            { 
+                result = num1 * num2;
+                Console.WriteLine("Result: {0} * {1} = {2}", num1, num2, num1 * num2);
+            }
+            else if (@operator == "/") 
+            { 
+                result = num1 / num2;
+                Console.WriteLine("Result: {0} / {1} = {2}", num1, num2, num1 / num2);
+            }
+
+            
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
